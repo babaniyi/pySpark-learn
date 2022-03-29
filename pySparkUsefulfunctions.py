3782909ffa106da68b20898729494e7aaa49dca5
Babaniyi Olaniyi <horlaneyee@gmail.com>
2022-03-29 11:06:49 +0200

    Update pySparkUsefulfunctions.py

--- pySparkUsefulfunctions.py
+++ pySparkUsefulfunctions.py
@@ -97,3 +97,11 @@ df.select("longitude", array_mean("longitude").alias("avg")).show()
 |    [-82.93, -82.93]|            -82.93|
 +--------------------+------------------+
 
+
+  #__________MELT Pypsark dataframe ________________
+def melt(df,cols,alias=('key','value')):
+  other = [col for col in df.columns if col not in cols]
+  for c in cols:
+    df = df.withColumn(c, F.expr(f'map("{c}", cast({c} as double))'))
+  df = df.withColumn('melted_cols', F.map_concat(*cols))
+  return df.select(*other,F.explode('melted_cols').alias(*alias))
